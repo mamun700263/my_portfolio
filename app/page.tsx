2453f5365f08d128da0e798cd625c52b7a7fff0b
MD ABDULLAH ALL MAMUN <mamun700263@gmail.com>
2025-12-03 10:34:42 +0000

    deployed to domain allmamun.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import AboutPage from "./about/page"
+
 export default function Home() {
   return (
     <main className="p-10 min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
